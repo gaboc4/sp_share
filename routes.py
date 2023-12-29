5a6301f1e8fcc7d94aec30cc79e1e86e1640842b
gabriel centeno <gabrielc007@gmail.com>
2023-12-28 21:41:35 -0600

    fast api working

--- routes.py
+++ routes.py
@@ -1,15 +1,16 @@
 import os
 import requests
+from typing import Optional
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 
 class SPAuth():
     def __init__(self) -> None:
         self.scope = "user-read-currently-playing"
-
         self.redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI')
         self.client_id = os.getenv('SPOTIPY_CLIENT_ID')
         self.client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
+        self.auth_url = f"https://accounts.spotify.com/authorize?response_type=code&client_id={sp_auth.client_id}&redirect_uri={sp_auth.redirect_uri}&scope={sp_auth.scope}"
     
     def get_access_token(self, auth_code: str):
         response = requests.post(
@@ -25,18 +26,20 @@ class SPAuth():
         return {"Authorization": "Bearer " + access_token}
 
 app = FastAPI()
+sp_auth = SPAuth()
 
-@app.route("/")
+
+@app.api_route("/")
 def home():
-    sp_auth = SPAuth()
-    auth_url = f"https://accounts.spotify.com/authorize?response_type=code&client_id={sp_auth.client_id}&redirect_uri={sp_auth.redirect_uri}&scope={sp_auth.scope}"
-    return HTMLResponse(content=f'<a href="{auth_url}">Authorize</a>')
+    return RedirectResponse(sp_auth.auth_url)
 
-@app.route("/get_song")
-def get_song(code):
-    sp_auth = SPAuth()
+@app.get("/get_song")
+def get_song(code: Optional[str] = None):
     headers = sp_auth.get_access_token(code)
     url = f"https://api.spotify.com/v1/me/player/currently-playing"
-    response = requests.post(url=url, headers=headers)
-    print(response)
-    return HTMLResponse(content=f'<p>bam</p>')
+    results = requests.get(url=url, headers=headers).json()
+    song_name = results['item']['name'] 
+    artist = results['item']['artists'][0]["name"]
+    preview_url = results['item']['artists'][0]['external_urls']['spotify']
+    image = results['item']['album']['images'][0]['url']
+    return HTMLResponse(content="\n".join([f"<img src={image}></img>", song_name, artist, preview_url]))
